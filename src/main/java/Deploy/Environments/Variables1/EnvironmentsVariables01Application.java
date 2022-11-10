08ef7e9b41062d9948e3d71aa41a20c91f1baef8
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-10 11:54:10 +0100

    Initial commit

---
+++ src/main/java/Deploy/Environments/Variables1/EnvironmentsVariables01Application.java
@@ -0,0 +1,13 @@
+package Deploy.Environments.Variables1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentsVariables01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentsVariables01Application.class, args);
+	}
+
+}
